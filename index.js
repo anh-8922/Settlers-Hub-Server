38860fb7067b063be85b8e38a72b2cf695a35831
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-07-25 07:38:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,10 +23,17 @@ const app = express();
 //    preflightContinue: true,
 //  })
 //);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://client-sh.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.use(cors({
-   origin: '*',
+   origin: 'https://client-sh.vercel.app',
    credentials: true,
    preflightContinue: true,
+   
  }))
 
 //const corsOptions = {
