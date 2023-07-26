8e6b7b08f8e2cd7d672c6d2dcf48c88bc1f5dad0
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-07-26 08:15:40 +0100

    updated

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express();
 //);
 
 app.use(cors({
-   origin: 'https://client-sh.vercel.app',
+   origin: ['https://client-sh.vercel.app', 'http://localhost:3000', "*"],
    credentials: true,
    preflightContinue: true,
    
